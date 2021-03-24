b262e463493702b9ee89ba542967c7f8fba8e6f2
Lennart Knüppel <lennart.knueppel@ue-germany.de>
2021-03-24 20:56:33 +0100

    added uni and a desk

---
+++ AltVrScenes/Assets/Plugins/uploadKit.bat
@@ -0,0 +1,2 @@
+echo off
+curl -v -b cookie -X PUT -F "kit[zip]=@D:\Studium\AltVRTemplaterRepo\AltVrScenes\1699321310682808552_utilitykit.zip" -F "kit[game_engine_version]=20194" https://account.altvr.com/api/kits/1699321310682808552.json
